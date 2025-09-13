d117bc9ed5b409e483d6a3517594d8b3d202b36b
jairjosafath <jairjosafath@gmail.com>
2025-09-13 14:21:38 +0200

    feat: Update scripts.sh to run Docker containers in detached mode for loader, mcp_server, extractor, and smart_stats services

--- scripts.sh
+++ scripts.sh
@@ -3,7 +3,7 @@ docker build -t smart_stats_loader:latest --build-arg service=loader .
 
 docker rm -f smart_stats_loader || true
 
-docker run --name smart_stats_loader -e HOST="http://host.docker.internal" -p 9998:8000 smart_stats_loader:latest
+docker run --name smart_stats_loader -d -e HOST="http://host.docker.internal" -p 9998:8000 smart_stats_loader:latest
 
 
 #mcp_server
@@ -11,13 +11,7 @@ docker build -t smart_stats_mcp_server:latest --build-arg service=mcp_server .
 
 docker rm -f smart_stats_mcp_server || true
 
-docker run -p 9999:8000 --name smart_stats_mcp_server smart_stats_mcp_server:latest
-
-
-#test
-docker build -t smart_stats_test:latest --build-arg service=test .
-
-docker run --name smart_stats_test smart_stats_test:latest
+docker run -d -p 9999:8000 --name smart_stats_mcp_server smart_stats_mcp_server:latest
 
 
 #extractor
@@ -25,7 +19,7 @@ docker build -t smart_stats_extractor:latest --build-arg service=extractor .
 
 docker rm -f smart_stats_extractor || true
 
-docker run --name smart_stats_extractor -e HOST="http://host.docker.internal" -p 9997:8000 smart_stats_extractor:latest
+docker run --name smart_stats_extractor -d -e HOST="http://host.docker.internal" -p 9997:8000 smart_stats_extractor:latest
 
 
 #smart_stats
@@ -33,7 +27,5 @@ docker build -t smart_stats:latest --build-arg service=smart_stats .
 
 docker rm -f smart_stats || true
 
-docker run --name smart_stats -e HOST="http://host.docker.internal" -p 8000:8000 smart_stats:latest
+docker run --name smart_stats -d -e HOST="http://host.docker.internal" -p 8000:8000 smart_stats:latest
 
-#inspector
-npx @modelcontextprotocol/inspector
